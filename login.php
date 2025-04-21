7e46237fd932ac78446888143fdfc0f0e89d4463
TomokiHashimoto13 <TomokiHashimoto13@gmail.com>
2025-04-21 15:46:11 +0900

    15:46

--- login.php
+++ login.php
@@ -1,3 +1,40 @@
+
+<?php
+session_start();
+require_once __DIR__ . "/../def.php";
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userID = trim($_POST['userID']);
+    $password = trim($_POST['password']);
+  try {
+    $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET;
+
+    $db = new PDO($dsn, DB_USER, DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $db->prepare("SELECT passwords FROM EMPLOYEE WHERE emp_no = :userID");
+
+    $stmt->bindParam(":userID", $userID, PDO::PARAM_STR);
+    
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC); //database
+    if ($user && password_verify($password, $user["passwords"])) {
+      $_SESSION["userID"] = $userID; //session 
+      // header("Location: "); 
+      // exit;
+    } else {
+      $error = "Invalid username or password!";
+    }
+  } catch (PDOException $e) {
+    exit("Database error: " . $e->getMessage());
+  }finally{
+    $stmt = null;
+    $db = null;
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,10 +51,10 @@
         </ul>
     </header>
     <main>
-        <form action="">
+        <form action="./login.php" method="POST">
             <div class="SUN">
                 <div class="fake-input">
-                    <input type="text" name="username" placeholder="username" />
+                    <input type="text" name="userID" placeholder="userID" />
                     <img src="./images/username.jpg" width=50 />
                 </div>
             </div>
@@ -27,7 +64,7 @@
                     <img src="./images/lock-icon-png-14.png" width=50 />
                 </div>
             </div>
-            <button id="B"><h1>LOGIN</h1></button>
+            <button type="submit" id="B"><h1>LOGIN</h1></button>
         </form>
         
           
